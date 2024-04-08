1020da134f3a9d5c789294212db790285acb0b24
IasminDev <iasmin.albr@gmail.com>
2024-04-08 19:51:25 -0300

    new node

--- react/pass-in-web/src/components/table/table-header.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentProps } from "react";
-
-interface TableHeaderProps extends ComponentProps<'th'>{}
-
-export function TableHeader(props: TableHeaderProps){
-    return(
-        <th {...props} className='py-3 px-4 text-sm font-semibold text-left'></th>
-    )
-}
